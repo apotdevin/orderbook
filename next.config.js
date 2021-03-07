cd018a36ce2891caeef09f56b3ddce2101c5bb68
apotdevin <potdevin.anthony@gmail.com>
2021-03-07 14:23:56 +0100

    chore: 🔧 cleanup url

--- next.config.js
+++ next.config.js
@@ -3,10 +3,8 @@ module.exports = {
   basePath: process.env.BASE_PATH || '',
   serverRuntimeConfig: {
     isProduction: process.env.NODE_ENV === 'production',
-    externalUrl:
-      process.env.EXTERNAL_URL || 'wss://www.cryptofacilities.com/ws/v1',
   },
   publicRuntimeConfig: {
-    wsUrl: process.env.WEBSOCKET_URL || 'ws://localhost:3000/api/ws',
+    wsUrl: process.env.WEBSOCKET_URL || 'wss://www.cryptofacilities.com/ws/v1',
   },
 };
